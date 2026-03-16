8b657b00786048360cae3dae04f6c6646c2e1057
anshulvyasa <anshulvtech121@gmail.com>
2026-03-16 14:00:08 +0000

    hair assesment hardcode page added

--- apps/web/app/(treatment-question)/hair-test/page.tsx
+++ apps/web/app/(treatment-question)/hair-test/page.tsx
@@ -3,34 +3,10 @@
 import { CurrentHairSection } from "@/components/hair/hair-current-section";
 import { PersonalInfoForm } from "@/components/hair/personal-form";
 import { Questions } from "@/components/hair/questions";
-import { readMajorIndex, readPersonalInfo, readQuestions } from "@/lib/redux/middlewareAndReaders";
 import { useMajorIndex } from "@/lib/redux/selectorAndDispatcher/useMajorIndex";
-import { usePersonalInfo } from "@/lib/redux/selectorAndDispatcher/usePersonalInfo";
-import { useQuestions } from "@/lib/redux/selectorAndDispatcher/useQuestions";
-import { useEffect, useState } from "react";
 
 const HairTreatement = () => {
-    const [loading, setLoading] = useState<boolean>(true);
-    const { majorIndex, initializeMajorIndex } = useMajorIndex();
-    const { initializePersonalInfo } = usePersonalInfo();
-    const { initializeQuestions, questionSelector } = useQuestions();
-
-
-    useEffect(() => {
-        const info = readPersonalInfo();
-        if (info) initializePersonalInfo(info);
-
-        const majorIndex = readMajorIndex();
-        if (majorIndex) initializeMajorIndex(majorIndex);
-
-        const questions = readQuestions();
-        if (questions) initializeQuestions(questions);
-
-        setLoading(false);
-    }, [])
-
-
-    if (loading) return <div>Loading...</div>
+    const { majorIndex} = useMajorIndex();
 
     return (
         <main className="px-30! overflow-hidden">
